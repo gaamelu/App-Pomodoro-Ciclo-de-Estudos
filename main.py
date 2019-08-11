12f2fdf94cea29e22d2788d0891fcb8514d63d9d
Drazyn <gabriellucena360@outlook.com>
2019-08-11 10:01:02 -0300

    Página de Ciclo de Estudos pronta
    Página com ScrollView, Título e pequena área para adição de itens ao ScrollView.

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-## Código desenvolvido por Gabriel Lucena (Drazyn)
+import kivy
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+    
+class Background(Widget):
+    def addTask(self):
+        if (self.ids.name.text == "" or not int(self.ids.time.text)):
+            print("ITEM SEM CONFIGURAÇÕES CORRETAS")
+            return
+        self.ids.box.add_widget(Button(text = "X", size_hint_x = None, width = 55, height = 55, size_hint_y = None))
+        self.ids.box.add_widget(Label(text = self.ids.name.text, size_hint_y = None, height = 55))    
+        self.ids.box.add_widget(Label(text = str(self.ids.time.text), size_hint_y = None, height = 55))
+        self.ids.name.text = ""
+        self.ids.time.text = ""
+        
+    pass     
+    
+class Tarefa(Widget):
+    pass
+
+class Inicializer(App):
+    def build(self):
+        background = Background()
+        return background
+        
+        
+if __name__ == "__main__":
+    Inicializer().run()
